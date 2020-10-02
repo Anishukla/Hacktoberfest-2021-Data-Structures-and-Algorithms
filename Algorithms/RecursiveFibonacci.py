987765853e258c1e2cff9f05937444f12a4d2cd9
Nidhi Bhanushali <71250816+NidhiBhanushali3701@users.noreply.github.com>
2020-10-02 10:31:47 +0530

    updated RecursiveFibonacci.py
    series are 0,1,1,2
    so if we go by position then for position 0 0 is answer
    for position 1 1 is answer
    for position 2 1 is answer
    
    Thank You!

--- Algorithms/RecursiveFibonacci.py
+++ Algorithms/RecursiveFibonacci.py
@@ -8,9 +8,9 @@ Fibonacci sequence value."""
 def fibonacci(position):
     if position == 0:
         return 0
-    elif position == 1:
+    elif position == 1 or position ==2 :
         return 1
-    elif position > 1:
+    elif position > 2:
         return fibonacci(position-1)+fibonacci(position-2)
     return -1
 

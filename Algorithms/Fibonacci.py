d70f23ec65029857232e81a7d6cdbd75014cc6a0
HaoES <hamza.essamaali@gmail.com>
2020-10-06 20:49:55 +0100

    Added Fibonacci implementation in Python.

--- Algorithms/Fibonacci.py
+++ Algorithms/Fibonacci.py
@@ -6,17 +6,17 @@ is exponential: O(2^n)
 The second implementation uses recurcive calls in a smart way to avoid redondancies.
 The complexity of this implementation is linear: O(n).
 """
-def bad_ﬁbonacci(n):
+def bad_fibonacci(n):
     #Return the nth Fibonacci number.
     if n <=1:
         return n
     else:
-        return bad_ﬁbonacci(n−2) + bad_ﬁbonacci(n−1)
+        return bad_fibonacci(n−2) + bad_fibonacci(n−1)
 
-def good_ﬁbonacci(n):
+def good_fibonacci(n):
     #Return pair of Fibonacci numbers, F(n) and F(n-1).
     if n <=1:
         return (n,0)
     else:
-        (a, b) = good_ﬁbonacci(n−1)
+        (a, b) = good_fibonacci(n−1)
         return (a+b, a)

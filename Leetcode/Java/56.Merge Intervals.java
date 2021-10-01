a535872267263bf893f46079e040e81520eade87
Rishit Chaudhary <rishit.c@live.com>
2021-10-02 03:06:40 +0530

    Added Java solution for Leetcode problem 56.

---
+++ Leetcode/Java/56.Merge Intervals.java
@@ -0,0 +1,19 @@
+// Time:  O(nlogn)
+// Space: O(n)
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        LinkedList<int[]> result = new LinkedList<>();
+        
+        for (int[] interval : intervals) {
+            if (result.isEmpty() || result.getLast()[1] < interval[0]) {
+                result.add(interval);
+            } else {
+                result.getLast()[1] = Math.max(result.getLast()[1], interval[1]);
+            }
+        }
+        
+        return result.toArray(new int[result.size()][]);
+    }
+}

69d0c1b26f403f6f8ead5ee18b98fcede3d7f0f4
Phalgun Mittal <phalgun99@gmail.com>
2021-10-31 22:01:31 +0530

    Create checkPalindrome.js

---
+++ Algorithms/Javascript/checkPalindrome.js
@@ -0,0 +1,17 @@
+// program to check if the string is palindrome or not
+
+function checkPalindrome(str) {
+
+    // find the length of a string
+    const len = string.length;
+
+    // loop through half of the string
+    for (let i = 0; i < len / 2; i++) {
+
+        // check if first and last string are same
+        if (string[i] !== string[len - 1 - i]) {
+            return 'It is not a palindrome';
+        }
+    }
+    return 'It is a palindrome';
+}
